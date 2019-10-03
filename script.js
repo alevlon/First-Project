62bd57036adc22a5ec54115c25da9494a5291e1f
alevlon <44722037+alevlon@users.noreply.github.com>
2019-10-03 20:58:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ $(document).ready(function()
 
     $(document).keydown(function(e) 
     {
-        if (e.keyCode == 37) rotateY -= 6;
-        if (e.keyCode == 38) rotateX += 6;
-        if (e.keyCode == 39) rotateY += 6;
-        if (e.keyCode == 40) rotateX -= 6;
+        if (e.keyCode == 37) rotateY -= 4;
+        if (e.keyCode == 38) rotateX += 4;
+        if (e.keyCode == 39) rotateY += 4;
+        if (e.keyCode == 40) rotateX -= 4;
 
 
         $('.wrap').css("transform", "rotateX(" + -rotateX + "deg) rotateY(" + -rotateY + "deg)");
